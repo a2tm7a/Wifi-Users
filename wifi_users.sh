ea32cbf56843962fb06c36075bfaa4871a0544d9
Amit Manchanda <amitdelhi1995@gmail.com>
2015-12-10 02:51:25 +0530

    Saved the MAC_Addresses in DB and updated the time for those already exist

--- wifi_users.sh
+++ wifi_users.sh
@@ -1,4 +1,22 @@
 #
 # To check for users over the wifi
 #
-sudo nmap -sP 192.168.1.0/24 | grep 'MAC Address' | awk '{print $3}'
+sudo nmap -sP 192.168.1.0/24 | grep 'MAC Address' | awk '{print $3}' > data.csv
+mysql --local-infile -uroot -pqwerty Home<<EOFMYSQL
+
+CREATE TEMPORARY TABLE temporary_detect LIKE Detect;
+
+LOAD DATA LOCAL INFILE 'data.csv'
+INTO TABLE temporary_detect
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+(MAC_Address);
+
+INSERT INTO Detect
+SELECT * FROM temporary_detect
+ON DUPLICATE KEY UPDATE Time = Detect.Time + 2;
+
+SELECT * FROM Detect;
+EOFMYSQL
+
+rm -f data.csv
+
